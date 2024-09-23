ff72bd9f5585be2f4117c655789647583ab301c8
Paul De Silos <paul_desilos@hotmail.com>
2024-09-23 03:45:26 -0500

    added @media tag, req work

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,3 +18,19 @@ module.exports = {
   darkMode: "class",
   plugins: [nextui()],
 }
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   theme: {
+//     screens: {
+//       'tablet': '640px',
+//       // => @media (min-width: 640px) { ... }
+
+//       'laptop': '1024px',
+//       // => @media (min-width: 1024px) { ... }
+
+//       'desktop': '1280px',
+//       // => @media (min-width: 1280px) { ... }
+//     },
+//   }
+// }
