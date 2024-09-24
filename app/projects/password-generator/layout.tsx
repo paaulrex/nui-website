6271e551db900f1e4135b8efeac2dcd35ca0c081
Paul De Silos <paul_desilos@hotmail.com>
2024-09-23 21:04:30 -0500

    rm moved code

--- app/projects/password-generator/layout.tsx
+++ app/projects/password-generator/layout.tsx
@@ -1,17 +1,4 @@
 import exp from "constants";
-import { Pixelify_Sans, Roboto } from "next/font/google";
-
-const pixel_init = Pixelify_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-pixelify",
-})
-
-const roboto_init = Roboto ({
-  subsets: ["latin"],
-  weight: ["100", "300", "500", "700"],
-  variable: "--font-roboto"
-})
 
 export default function ProjectsLayout({
     children,
@@ -19,7 +6,7 @@ export default function ProjectsLayout({
     children: React.ReactNode;
   }) {
     return (
-      <section className={`${pixel_init.variable} project-sect ${roboto_init.variable}`}>
+      <section className="project-sect">
           {children}
       </section>
     );
