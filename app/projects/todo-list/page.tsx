4bc406b4fe0cfbc4c20f41fa0cbc96e3da2d1f5b
Paul De Silos <pauldesilos@yahoo.com>
2024-09-30 11:50:55 -0500

    great stopping pt, added firebase fetch and push + list ui for "anime" -- needs more coding for hidden and block css for anime ui div

--- app/projects/todo-list/page.tsx
+++ app/projects/todo-list/page.tsx
@@ -1,37 +1,146 @@
 "use client"
 
 import { title } from "@/components/primitives"
-import { Card, CardBody, CardFooter } from "@nextui-org/card";
-import { Link } from "@nextui-org/link";
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
+import { onValue, push, ref, remove, set  } from "firebase/database";
+import db from "./firebaseConfig.js";
+import Link from "next/link";
+
 import Image from "next/image";
 import animePic from "@/components/images/list-anime.png"
 import tvPic from "@/components/images/list-tv.png"
 import moviePic from "@/components/images/list-movies.png"
 import vGamesPics from "@/components/images/list-vg.png"
+import { use, useEffect, useState } from "react";
 
 export default function TodoListPage() {
+  const [animeName, setAnimeName] = useState("")
+  const [animeList, setAnimeList] = useState({})
+
+  const [tvName, setTvName] = useState("")
+  const [tvShowList, setTvShowList] = useState({})
+  
+  const [movieName, setMovieName] = useState("")
+  const [movieList, setMovieList] = useState({})
+
+  const [vgName, setVgName] = useState("")
+  const [vgList, setVgList] = useState({})
+
+  const addToList = async () => {
+    try {
+      if (animeName) {
+        const animeRef = ref(db, "anime")
+        const newAnimeData = push(animeRef)
+        await set(newAnimeData, {
+          anime_name: animeName
+        })
+      }
+
+      if (tvName) {
+        const tvRef = ref(db, "tv-shows")
+        const newTvData = push(tvRef)
+        await set(newTvData, {
+          tvshow_name: tvName
+        })
+      }
+
+      if (movieName) {
+        const movieRef = ref(db, "movies")
+        const newMovieData = push(movieRef)
+        await set(newMovieData, {
+          movie_name: movieName
+        })
+      }
+
+      if (vgName) {
+        const vgRef = ref(db, "video-games")
+        const newVgData = push(vgRef)
+        await set(newVgData, {
+          videogame_name: vgName
+        })
+      }
+
+      alert("Item has been added to your list")
+      window.location.reload()
+
+    } catch (error) {
+      console.error("Firebase Error", error)
+    }
+  }
+
+  // Get Anime List
+  useEffect(() => {
+    const getAnimeList = async () => {
+      const animeRef = ref(db, "anime")
+      onValue(animeRef, (snapshot) => {
+        if (!snapshot.exists()) return
+        let animeList = snapshot.val()
+        setAnimeList(animeList)
+      })
+    }
+    getAnimeList()
+  }, [])
+
+  // Get TV Show List
+  useEffect(() => {
+    const getTvShowList = async () => {
+      const tvShowRef = ref(db, "tv-show")
+      onValue(tvShowRef, (snapshot) => {
+        if (!snapshot.exists()) return
+        let tvShowList = snapshot.val()
+        setTvShowList(tvShowList)
+      })
+    }
+    getTvShowList()
+  }, [])
+
+  // Get Movies List
+  useEffect(() => {
+    const getMovieList = async () => {
+      const movieRef = ref(db, "movies")
+      onValue(movieRef, (snapshot) => {
+        if (!snapshot.exists()) return
+        let movieList = snapshot.val()
+        setMovieList(movieList)
+      })
+    }
+    getMovieList()
+  }, [])
+
+  // Get Video Games List
+  useEffect(() => {
+    const getVgList = async () => {
+      const vgRef = ref(db, "video-games")
+      onValue(vgRef, (snapshot) => {
+        if (!snapshot.exists()) return
+        let vgList = snapshot.val()
+        setVgList(vgList)
+      })
+    }
+    getVgList()
+  }, [])
+
   const list =[
     {
         title: "Anime",
-        href: "todo-list/anime",
         img: animePic,
     },
     {
         title: "TV Shows",
-        href: "todo-list/tvshows",
         img: tvPic
     },
     {
         title: "Movies",
-        href: "todo-list/movies",
         img: moviePic,
     },
     {
         title: "Video Games",
-        href: "todo-list/videogames",
         img: vGamesPics,
     },
   ]
+
   return (
   <div className="roboto  ">
     <div className="jbmono">
@@ -41,23 +150,70 @@ export default function TodoListPage() {
 
     <div className="gap-5 grid grid-cols-2 sm:grid-cols-2 p-5">
       {list.map((item, index) => (
-      <Link href={item.href}>
-        <Card key={index}>
-          <CardBody className="overflow-visible">
-            <Image
-              width={100}
-              height={100}
-              alt={item.title}
-              className="w-full rounded-full"
-              src={item.img}
-              />
-          </CardBody>
-          <CardFooter className="text-small justify-center">
-            <p className="text-default-500">{item.title}</p>
-          </CardFooter>
-        </Card>
-      </Link>
+        <div>
+          <Card key={index} isPressable>
+            <CardBody className="overflow-visible">
+              <Image
+                width={100}
+                height={100}
+                alt={item.title}
+                className="w-full rounded-full"
+                src={item.img}
+                />
+            </CardBody>
+            <CardFooter className="text-small justify-center">
+              <p className="text-default-500">{item.title}</p>
+            </CardFooter>
+          </Card>
+        </div>
       ))}
+    </div>
+
+    {/* Template for list */}
+    <div>
+      <Card className="gap-1">
+        <CardHeader className="justify-center">
+          <Link href="./todo-list">
+            <Button>
+              Back to List
+            </Button>
+          </Link>
+        </CardHeader>
+        <CardBody className="gap-5 justify-center items-center">
+          <Image 
+            width={100}
+            height={100}
+            alt={list[0].title}
+            src={list[0].img}
+            className="rounded-full"
+          />
+          <Input
+            type="text"
+            label="Anime"
+            placeholder="Solo Leveling"
+            className="max-w-xs"
+            onChange={(e) => setAnimeName(e.target.value)}
+          />
+        </CardBody>
+        <CardFooter className="justify-center">
+          <Button 
+            
+            className="w-full"
+            onPress={() => addToList()}
+          >
+            Add to List</Button>
+        </CardFooter>
+      </Card>
+      
+      <div className="w-[400px] flex flex-wrap justify-center gap-2 p-5">
+        {Object.keys(animeList).map((item) => (
+            <Button className="flex-grow">
+              { // @ts-ignore
+              animeList[item]?.anime_name}
+            </Button>
+        ))}
+      </div>
+
     </div>
   </div>
   );
