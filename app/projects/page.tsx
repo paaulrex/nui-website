7636513635746ab54fa8762cc0f1b9e76f8746d9
Paul De Silos <pauldesilos@yahoo.com>
2024-12-15 02:33:01 -0600

    added another project button --req new picture/using PG pic as placeholder

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -17,6 +17,30 @@ export default function ProjectsPage(){
       <Divider className="my-5" />
       <div>
         <Button className="h-auto" variant="ghost">
+          <Link href="projects/calculator-app">
+            <Card
+              isBlurred
+              radius="lg"
+              className="pr-card"
+            >
+            <Image
+              alt="Calculator App Picture"
+              className="object-cover pr-img"
+              height={400}
+              src={passGenPic}
+              width={400}
+            />
+              <CardBody
+                className="justify-center items-center bg-white/80 before:bg-black/10 border-black/20 border-1 overflow-hidden absolute before:rounded-xl rounded-large bottom-1/2 w-3/4 mx-12 z-10 p-auto">
+                <p className="text-md font-bold text-black/80 jbmono text-center pr-msg">Calculator App</p>
+              </CardBody>
+            </Card>
+          </Link>
+        </Button>
+
+        <Divider className="my-5" />
+
+        <Button className="h-auto" variant="ghost">
           <Link href="projects/password-generator">
             <Card
               isBlurred
